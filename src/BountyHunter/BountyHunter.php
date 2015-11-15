706d08e98f68141d5d16b12ddab57b4d235b2337
DevCraftProductions <DevCraftProductions@users.noreply.github.com>
2015-11-15 14:02:27 -0600

    Update BountyHunter.php

--- src/BountyHunter/BountyHunter.php
+++ src/BountyHunter/BountyHunter.php
@@ -1,3 +1,15 @@
 <?php
 
+namespace BountyHunter\BountyHunter
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+}
+
 ?>
